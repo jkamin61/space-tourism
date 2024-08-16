6c8e488fc36633f32a0d381981a9baaae4f00415
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-08-16 15:18:57 +0200

    font set up

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,34 +3,17 @@
 import React, {useEffect, useRef, useState} from "react";
 import Image from "next/image";
 import logo from "../../public/Subtract.svg";
+import menu from "../../public/shared/icon-hamburger.svg"
 
 const Navigation: React.FC = () => {
 
-    const ref = useRef<HTMLElement>(null);
-    const [isIntersecting, setIntersecting] = useState(true);
-
-    useEffect(() => {
-        if (!ref.current) return;
-        const observer = new IntersectionObserver(([entry]) =>
-            setIntersecting(entry.isIntersecting),
-        );
-
-        observer.observe(ref.current);
-        return () => observer.disconnect();
-    }, []);
-
     return (
-        <header>
-            <div
-                className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b  ${
-                    isIntersecting
-                        ? "bg-zinc-900/0 border-transparent"
-                        : "bg-zinc-900/500 border-zinc-800"
-                }`}
-            >
-                <div className="bg-black">
-                    <Image src={logo} alt="Logo"/>
-                </div>
+        <header className="flex flex-row justify-between items-center p-6">
+            <div>
+                <Image src={logo} alt="Logo"/>
+            </div>
+            <div>
+                <Image src={menu} className="hover:cursor-pointer" alt="menu icon"/>
             </div>
         </header>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,30 @@ import Image from "next/image";
 import Navigation from "@/app/components/nav";
 
 export default function Home() {
-  return (
-   <Navigation></Navigation>
-  );
+    return (
+        <div className="min-h-screen min-w-full bg-cover bg-space-pattern">
+            <Navigation></Navigation>
+            <main className="flex flex-col justify-center items-center text-white h-full">
+                <div className="flex flex-col w-full items-center justify-center p-6">
+                    <h3 className="mt-6 text-md font-barlow-condensed font-light tracking-wider">SO, YOU WANT TO TRAVEL
+                        TO</h3>
+                    <h1 className="my-6 font-bellefair font-thin font text-7xl tracking-tight">SPACE</h1>
+                    <p className="text-center font-light text-md">Let’s face it; if you want to go to space, you might
+                        as
+                        well
+                        genuinely go
+                        to outer
+                        space and not
+                        hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of
+                        this world experience!</p>
+                </div>
+                <div
+                    className="my-16 w-24 h-24 shrink-0 grow-0 rounded-full bg-white items-center flex justify-center hover:animate-pulse focus:animate-pulse ease-out">
+                    <button
+                        className="font-bellefair text-black text-md">EXPLORE
+                    </button>
+                </div>
+            </main>
+        </div>
+    );
 }
