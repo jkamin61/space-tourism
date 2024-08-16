e90a0d0af5f2af3b633d5e85b6718e1342fca6ab
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-08-16 23:30:17 +0200

    destination page structure

---
+++ app/destination/page.tsx
@@ -0,0 +1,14 @@
+import Navigation from "@/app/components/nav";
+
+
+export default function Destination() {
+
+    return (
+        <div className="min-h-screen min-w-full bg-cover bg-space-pattern-mobile">
+            <Navigation/>
+            <main>
+
+            </main>
+        </div>
+    )
+}
